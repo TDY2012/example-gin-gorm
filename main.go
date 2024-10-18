b60e5519bb8219339e77cc4cdf5826485e436a72
Thitiwudh Uasmith <tdy2020@gmail.com>
2024-10-18 12:47:17 +0700

    feat: Added CRUD apis.

--- main.go
+++ main.go
@@ -22,6 +22,8 @@ type ToDo struct {
 }
 
 func main() {
+	var err error
+
 	// Get database environment variables.
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
@@ -31,19 +33,24 @@ func main() {
 
 	// Try connecting to the database.
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", dbHost, dbUser, dbPassword, dbName, dbPort)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	// Migrate the schema
+	// Migrate the schema.
 	db.AutoMigrate(&ToDo{})
 
-	db.Create(&ToDo{Content: "Study Go.", IsDone: true})
+	// Populate sample data.
+	// db.Create(&ToDo{Content: "Study Go.", IsDone: true})
 
 	// Initialize app router.
 	router := gin.Default()
 	router.GET("/todos", getToDos)
+	router.POST("/todo", createToDo)
+	router.GET("/todo/:id", getToDo)
+	router.PUT("/todo/:id", updateToDo)
+	router.DELETE("/todo/:id", deleteToDo)
 
 	router.Run("localhost:8080")
 }
@@ -52,4 +59,38 @@ func getToDos(c *gin.Context) {
 	var toDos []ToDo
 	db.Find(&toDos)
 	c.IndentedJSON(http.StatusOK, toDos)
+}
+
+func createToDo(c *gin.Context) {
+	var toDo ToDo
+	c.BindJSON(&toDo)
+	db.Create(&toDo)
+	c.IndentedJSON(http.StatusOK, toDo)
+}
+
+func getToDo(c *gin.Context) {
+	// Theses parameters are path parameters.
+	id := c.Param("id")
+	var toDo ToDo
+
+	// Without column name specified, GORM uses primary key which is ID.
+	db.First(&toDo, id)
+	c.IndentedJSON(http.StatusOK, toDo)
+}
+
+func updateToDo(c *gin.Context) {
+	id := c.Param("id")
+	var toDo ToDo
+	db.First(&toDo, id)
+	c.BindJSON(&toDo)
+	db.Save(&toDo)
+	c.IndentedJSON(http.StatusOK, toDo)
+}
+
+func deleteToDo(c *gin.Context) {
+	id := c.Param("id")
+	var toDo ToDo
+	db.First(&toDo, id)
+	db.Delete(&toDo)
+	c.IndentedJSON(http.StatusOK, toDo)
 }
